7d2580d70d94ff7168ec8a32f316e5d2d87803d9
kimrobbin <156893524+kimrobbin@users.noreply.github.com>
2025-11-12 09:39:49 +0100

    lagde forekomster. har input for tekst og hvilket tegn

--- telling.py
+++ telling.py
@@ -1,1 +1,14 @@
 
+def forekomster():
+   tekst = input("Skriv noe: ") 
+   tegn = input("Hvilket tegn? ")
+   antall_tegn = 0
+
+   for i in tekst:
+    if i == tegn:
+        antall_tegn += 1
+        
+            
+   print(antall_tegn)
+
+forekomster()
